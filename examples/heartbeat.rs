b3e08026fdd7e82f62b96d6c83ae4d2360d50d7c
skrrb <skrrb@pm.me>
2025-01-20 13:08:29 +0100

    fix heartbeat example

--- examples/heartbeat.rs
+++ examples/heartbeat.rs
@@ -1,11 +1,8 @@
 #[tokio::main]
 async fn main() -> better_uptime::Result<()> {
     // https://uptime.betterstack.com/api/v1/heartbeat/XXX, the XXX part is the identifier
-    let identifier = "XXX".to_string();
-    let uptime = better_uptime::Uptime {
-        token: "".to_string(), // We can ignore this since heartbeat doesn't use it
-    };
-    uptime.heartbeat(identifier).await?;
+    let identifier = "XXX";
+    better_uptime::Uptime::heartbeat(identifier).await?;
 
     Ok(())
 }
